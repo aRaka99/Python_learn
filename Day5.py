71bea0b2ddbee1fac0e00ecf2c6268f5c337e8a9
aRaka99 <178140988+aRaka99@users.noreply.github.com>
2025-05-08 09:14:49 +0000

    day5

---
+++ Day5.py
@@ -0,0 +1,7 @@
+# code every interpret by python interpret
+# comment single line comment(#) ctrl + /
+# multiple line (''' comment ''')
+'''this is multiline comment'''
+# escape sequence of print statment
+# escape sequnce chararacter: (\n )
+print("hey i am good boy")
