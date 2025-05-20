12861a584423295b5a5b4ed74dbedaef49c5ca8c
aRaka99 <178140988+aRaka99@users.noreply.github.com>
2025-05-20 11:01:35 +0000

    String Slicing

---
+++ Day11.py
@@ -0,0 +1,13 @@
+# this is string day here we learn string
+# single quote and double quotes
+name= "shiva"
+friend='yes'
+# what if we have to used " " in string we enclosed in '''__''' triple single quotes
+str = ''' yes i am this "do" done'''
+
+# indexing 0,1,2,3,n
+print(friend[0])
+# if there is no element so its give an error
+# we can loop through strings using for loop in strings
+
+ 
---
+++ Day12.py
@@ -0,0 +1,16 @@
+# string slicing
+name="shivam,maur"
+# we can find using length of a string
+print(len(name))
+
+# slicing
+print(name[0:4])
+print(name[:4])
+print(name[1:4])
+print(name[:])
+# negative slicing
+print(name[0:-3]) #negative len minus from -3
+
+# quick quiz
+nm = "harry"
+print(nm[-4:-2])
