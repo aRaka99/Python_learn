d7a06d4a5e7d148eb2bece8ac1c949c6f155d564
aRaka99 <178140988+aRaka99@users.noreply.github.com>
2025-06-11 10:06:28 +0000

    day42

---
+++ Day42.py
@@ -0,0 +1,17 @@
+# enumerte function 
+# to remove   temprory
+# enumerate function used on list string tupple and first vlue is index anf second is value
+marks= [4,52,48,65,12,94,56]
+# index=0
+# for mark in marks:
+#     print(mark)
+#     if (index == 3):
+#         print("you are osmm")
+#     index +=1
+
+
+for index, mark in enumerate(marks):
+    
+    if (index==2):
+        print(f"our marks is {mark}")
+    print(index,mark)
